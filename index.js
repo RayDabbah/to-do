878fbf1783a14900b428fb0578ee034cd06d2106
raydabbah <dabbahray@yahoo.com>
2017-07-26 23:13:30 -0400

    conncted mongoDb

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const app = express();
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
+mongoose.connect("mongodb://localhost/toDoApp");
 
 app.listen(port, ()=> console.log(`Server is listening on port ${port}.`))
